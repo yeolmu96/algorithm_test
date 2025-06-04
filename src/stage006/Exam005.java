c91738c27373aca8a75a2fcda49928b78d2a96be
yeolmu96 <jeong7653@naver.com>
2025-06-04 17:04:28 +0900

    [정은선] stage6_exam5 ing

---
+++ src/stage006/Exam005.java
@@ -0,0 +1,24 @@
+package stage006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 백준_10988번
+ * https://www.acmicpc.net/problem/10988
+ * 
+ */
+public class Exam005 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		str.toLowerCase();
+		char[] ch = str.toCharArray();
+		
+		for(int i = 0; i < ch.length; i++) {
+			
+		}
+	}
+}
