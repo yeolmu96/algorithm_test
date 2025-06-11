fdaed7ecde1a986dbc6fc4e49239ac1f40ec2f7e
yeolmu96 <jeong7653@naver.com>
2025-06-12 08:12:54 +0900

    [정은선]Update stage6_exam7,8

---
+++ src/stage006/Exam007.java
@@ -0,0 +1,26 @@
+package stage006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 백준_1316번
+ * https://www.acmicpc.net/problem/1316
+ * replace()로 치환
+ * 
+ */
+public class Exam007 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] data = new String[n];
+		
+		for(int i = 0; i < n; i++) {
+			data[i] = br.readLine();
+		}
+		
+		
+	}
+}
---
+++ src/stage006/Exam008.java
@@ -0,0 +1,13 @@
+package stage006;
+
+/*
+ * 백준_225206번
+ * https://www.acmicpc.net/problem/25206
+ * 
+ */
+public class Exam008 {
+
+	public static void main(String[] args) {
+		
+	}
+}
