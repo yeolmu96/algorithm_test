5a5dbfac699c4bf058bc62df3a2e42a9b513b730
yeolmu96 <jeong7653@naver.com>
2026-02-11 20:31:49 +0900

    [정은선] BOJ_2753_윤년

---
+++ src/practice_0210/BOJ_2753.java
@@ -0,0 +1,34 @@
+package practice_0210;
+
+/*
+2753번
+윤년 : (4의 배수 && 100의 배수 아님) OR 400의 배수
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2753 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)){
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+
+//        if((n % 4) == 0) {
+//            if((n % 100) != 0 || (n % 400) == 0) {
+//                System.out.println(1);
+//            } else {
+//                System.out.println(0);
+//            }
+//        } else {
+//            System.out.println(0);
+//        }
+    }
+}
